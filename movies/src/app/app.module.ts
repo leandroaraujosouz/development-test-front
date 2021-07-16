059b9a6e0b31698f0f7367981ee428f7384a374e
leandro souza <leandro_a_souza@hotmail.com>
2021-07-15 22:21:47 -0300

    correção de rotas

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MoviesComponent } from './movies/movies.component';
 import { MovieDeleteComponent } from './delete/movie-delete/movie-delete.component';
 import { MovieEditComponent } from './edit/movie-edit/movie-edit.component';
 import { CadastrarMovieComponent } from './cadastrar-movie/cadastrar-movie.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,7 @@ import { CadastrarMovieComponent } from './cadastrar-movie/cadastrar-movie.compo
     FormsModule,
     ModalModule.forRoot(),
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
